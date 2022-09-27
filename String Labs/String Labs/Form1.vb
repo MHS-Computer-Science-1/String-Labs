f702717493f7c53e287d2335ff3875967f7a56c2
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-09-27 15:36:25 -0400

    Add files via upload

---
+++ String Labs/String Labs/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Madlib
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Fill in the blanks 1
+        Dim quote As String
+        quote = "How you spend your days is how you spend your life."
+
+        'Find the index of the period and add it to the ListBox
+
+        'Find the index of where "your" first appears and add it to the ListBox
+
+        'Find the index where "your" last appears and add it to the ListBox
+
+        'Find the index where "zebra" appears and add it to the ListBox 
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Email
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Fill in the blanks 2
+        Dim quote As String
+        quote = "Failure is not the opposite of success, it's part of it."
+
+        Dim result1, result2, result3, result4, result5 As String
+
+        'Extract and print "Failure"
+
+        'result1 = quote.Substring(, )
+        ListBox1.Items.Add(result1)
+
+        'Extract and print "is"
+
+        'result2 =
+        ListBox1.Items.Add(result2)
+
+        'Extract and print "part"
+
+        'result3 =
+        ListBox1.Items.Add(result3)
+
+        'Extract and print "of"
+
+        'result4 = 0
+        ListBox1.Items.Add(result4)
+
+        'Extract and print "success"
+
+        'result5 = 0
+        ListBox1.Items.Add(result5)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Username 
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Word Count
+
+    End Sub
+End Class
